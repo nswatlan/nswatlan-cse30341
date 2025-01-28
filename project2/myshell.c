ff81926d201a36c2973644cff86982fd7ded668b
Norah Swatland <nswatlan@nd.edu>
2025-01-28 11:28:29 -0500

    updating makefile/preliminary code

--- project2/myshell.c
+++ project2/myshell.c
@@ -1,3 +1,39 @@
 //Norah Swatland
 //CSE 30341 Project 2 
 
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <errno.h>
+
+#define MAX 100
+//prototypes
+
+
+//main
+int main() {
+    char line[100]; 
+    while(1) {
+        printf("myshell> "); 
+        fgets(line, MAX, stdin); 
+        printf("%s", line); 
+        break; 
+    }
+
+
+
+    return 0; 
+}
+
+
+
+//function definitions
+
+
